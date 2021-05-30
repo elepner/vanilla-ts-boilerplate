05df2f63cd7e7507c27193a4c308099b1a52b629
Eduard Lepner <eduard@invarion.com>
2021-05-30 12:31:25 +0200

    WIP

---
+++ app/simple-test.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('My fancy test', () => {
+  it('should work correctly', () => {
+
+  });
+});
--- index.ts
+++ index.ts
@@ -1,4 +1,19 @@
 import { appendElement } from './app/test';
 
 const el = document.getElementsByTagName('body')[0];
+
+const inc = (function incFn() {
+  let counter = 0;
+  return () => {
+    return counter++;
+  }
+})();
+
+inc()
+inc()
+inc()
+inc()
+
+
+
 appendElement(el);
